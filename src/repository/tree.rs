b89363a90d4b91206118fe866f64d5f9713dea5f
Javier Lancha Vázquez <javier.lancha@gmail.com>
2025-04-28 03:11:00 +0200

    FilesystemNodeStreamer

--- src/repository/tree.rs
+++ src/repository/tree.rs
@@ -14,12 +14,13 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+use std::path::{Path, PathBuf};
 use std::{fs::DirEntry, time::SystemTime};
 
 #[cfg(unix)]
 use std::os::unix::fs::MetadataExt;
 
-use anyhow::{Context, Result, bail};
+use anyhow::{Context, Result, anyhow, bail};
 use serde::{Deserialize, Serialize};
 
 use super::backend::{BlobId, TreeId};
@@ -76,7 +77,7 @@ impl Metadata {
 pub enum NodeType {
     File,
     Directory,
-    Simlink,
+    Symlink,
 }
 
 /// A Node, representing an item in a filesystem.
@@ -110,7 +111,7 @@ impl Node {
         } else if os_meta.is_file() {
             NodeType::File
         } else if os_meta.is_symlink() {
-            NodeType::Simlink
+            NodeType::Symlink
         } else {
             bail!(
                 "Unsupported dir entry encountered for path \'{}\'",
@@ -127,12 +128,19 @@ impl Node {
         })
     }
 
+    /// Returns true if the node is a directory
     pub fn is_dir(&self) -> bool {
         self.node_type == NodeType::Directory
     }
 
+    /// Returns true if the node is a file
     pub fn is_file(&self) -> bool {
         self.node_type == NodeType::File
+    }
+
+    /// Returns true if the node is a symlink
+    pub fn is_symlink(&self) -> bool {
+        self.node_type == NodeType::Symlink
     }
 }
 
@@ -140,4 +148,106 @@ impl Node {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Tree {
     pub nodes: Vec<Node>,
+}
+
+pub struct FilesystemNodeStreamer {
+    stack: Vec<PathBuf>,
+}
+
+impl FilesystemNodeStreamer {
+    pub fn new(root_path: &Path) -> Result<Self> {
+        if !root_path.exists() {
+            bail!(
+                "Failed to create streamer for path \'{}\'. The path doesn't exist.",
+                root_path.display()
+            );
+        }
+
+        Ok(FilesystemNodeStreamer {
+            stack: vec![root_path.to_path_buf()],
+        })
+    }
+
+    fn sort_dir_entries(dir_path: &Path) -> Result<Vec<PathBuf>> {
+        let read_dir_iterator = std::fs::read_dir(dir_path)
+            .with_context(|| format!("Failed to read directory: {}", dir_path.display()))?;
+
+        let mut entries: Vec<PathBuf> = Vec::new();
+
+        for entry_result in read_dir_iterator {
+            let entry = entry_result.with_context(|| {
+                format!(
+                    "Failed to read an entry in directory: {}",
+                    dir_path.display()
+                )
+            })?;
+            entries.push(entry.path());
+        }
+
+        let mut sorted_entries = entries;
+        sorted_entries.sort_by(|first, second| first.file_name().cmp(&second.file_name()));
+
+        Ok(sorted_entries)
+    }
+}
+
+impl Iterator for FilesystemNodeStreamer {
+    type Item = Result<(PathBuf, Node)>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.stack.pop() {
+            None => None, // The stack is empty. We are finished.
+            Some(path) => {
+                let name = path
+                    .file_name()
+                    .map(|o| o.to_string_lossy().into_owned())
+                    .unwrap();
+
+                let os_meta = match std::fs::metadata(&path) {
+                    Ok(meta) => meta,
+                    Err(_) => {
+                        return Some(Err(anyhow!(
+                            "Failed to read metadata for path \'{}\'",
+                            path.display()
+                        )));
+                    }
+                };
+
+                let node_type = if os_meta.is_dir() {
+                    // Push children to the stack in reverse alphabetical order
+                    let sorted_children = Self::sort_dir_entries(&path);
+                    match sorted_children {
+                        Ok(list) => self.stack.extend(list.into_iter().rev()),
+                        Err(_) => {
+                            return Some(Err(anyhow!(
+                                "Failed to list directory \'{}\'",
+                                &path.display()
+                            )));
+                        }
+                    }
+
+                    NodeType::Directory
+                } else if os_meta.is_file() {
+                    NodeType::File
+                } else if os_meta.is_symlink() {
+                    NodeType::Symlink
+                } else {
+                    return Some(Err(anyhow!(
+                        "Unsupported dir entry encountered for path \'{}\'",
+                        &path.display()
+                    )));
+                };
+
+                let node = Node {
+                    name,
+                    node_type: node_type,
+                    metadata: Metadata::from_fs_meta(&os_meta),
+                    contents: None,
+                    tree: None,
+                };
+
+                Some(Ok((path, node)))
+            }
+        }
+    }
 }

9fdc7d2ab14438bb632ae3b252ef257911f5a755
Javier Lancha Vázquez <javier.lancha@gmail.com>
2025-07-12 00:07:52 +0200

    Add progress bar to verify cmd

--- src/commands/cmd_verify.rs
+++ src/commands/cmd_verify.rs
@@ -14,18 +14,23 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use std::collections::BTreeSet;
+use std::{collections::BTreeSet, path::PathBuf, sync::Arc};
 
-use anyhow::Result;
+use anyhow::{Result, bail};
 use clap::Args;
 use colored::Colorize;
+use indicatif::{ProgressBar, ProgressState, ProgressStyle};
 
 use crate::{
     backend::new_backend_with_prompt,
     commands::GlobalArgs,
-    global::defaults::SHORT_SNAPSHOT_ID_LEN,
-    repository::{self, snapshot::SnapshotStreamer, verify::verify_snapshot},
-    ui, utils,
+    global::{ID, defaults::SHORT_SNAPSHOT_ID_LEN},
+    repository::{
+        self, RepositoryBackend, snapshot::SnapshotStreamer, streamers::SerializedNodeStreamer,
+        tree::NodeType, verify::verify_blob,
+    },
+    ui::{self, default_bar_draw_target},
+    utils,
 };
 
 #[derive(Args, Debug)]
@@ -77,6 +82,88 @@ pub fn run(global_args: &GlobalArgs, _args: &CmdArgs) -> Result<()> {
     }
     if error_counter > 0 {
         ui::cli::log!("{} {}", error_counter, "[ERROR]".bold().red());
+    }
+
+    Ok(())
+}
+
+pub fn verify_snapshot(
+    repo: Arc<dyn RepositoryBackend>,
+    snapshot_id: &ID,
+    visited_blobs: &mut BTreeSet<ID>,
+) -> Result<()> {
+    let snapshot_data = repo.load_file(crate::global::FileType::Snapshot, snapshot_id)?;
+    let checksum = utils::calculate_hash(snapshot_data);
+    if checksum != snapshot_id.0[..] {
+        bail!("Invalid snapshot checksum");
+    }
+
+    let snapshot = repo.load_snapshot(snapshot_id)?;
+    let tree_id = snapshot.tree.clone();
+    let streamer =
+        SerializedNodeStreamer::new(repo.clone(), Some(tree_id), PathBuf::new(), None, None)?;
+
+    let bar = ProgressBar::new(snapshot.size());
+    bar.set_draw_target(default_bar_draw_target());
+    bar.set_style(
+        ProgressStyle::default_bar()
+            .template("[{custom_elapsed}] [{bar:20.cyan/white}] {processed_bytes_formated} {msg}")
+            .unwrap()
+            .progress_chars("=> ")
+            .with_key(
+                "custom_elapsed",
+                move |state: &ProgressState, w: &mut dyn std::fmt::Write| {
+                    let elapsed = state.elapsed();
+                    let custom_elapsed = utils::pretty_print_duration(elapsed);
+                    let _ = w.write_str(&custom_elapsed);
+                },
+            )
+            .with_key(
+                "processed_bytes_formated",
+                move |state: &ProgressState, w: &mut dyn std::fmt::Write| {
+                    let s = format!(
+                        "{} / {}",
+                        utils::format_size(state.pos(), 3),
+                        utils::format_size(state.len().unwrap(), 3)
+                    );
+                    let _ = w.write_str(&s);
+                },
+            ),
+    );
+
+    let mut error_counter = 0;
+    for (_path, stream_node) in streamer.flatten() {
+        let node = stream_node.node;
+        match node.node_type {
+            NodeType::File => {
+                if let Some(blobs) = node.blobs {
+                    for blob in blobs {
+                        if !visited_blobs.contains(&blob) {
+                            visited_blobs.insert(blob.clone());
+                            match verify_blob(repo.as_ref(), &blob, None) {
+                                Ok(blob_len) => bar.inc(blob_len),
+                                Err(_) => {
+                                    error_counter += 1;
+                                    bar.set_message("{error_counter} errors");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            NodeType::Symlink
+            | NodeType::Directory
+            | NodeType::BlockDevice
+            | NodeType::CharDevice
+            | NodeType::Fifo
+            | NodeType::Socket => (),
+        }
+    }
+
+    bar.finish_and_clear();
+
+    if error_counter > 0 {
+        bail!("Snapshot has {} corrupt blobs", error_counter);
     }
 
     Ok(())

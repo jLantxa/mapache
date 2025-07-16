3617f2c06ab8938c21714f635befbd245ae4b09b
Javier Lancha Vázquez <javier.lancha@gmail.com>
2025-07-17 00:13:00 +0200

    Migrate from AES-GCM to AES-GCM-SIV
    Switched to AES-GCM-SIV to gain nonce misuse resistance. This prevents
    catastrophic security failures in the event of accidental IV reuse,
    which is a risk when generating random IVs.
    
    SecureStorage generates a random IV for every encoded message. The
    probability of a collission is astronomically low, but still greater
    than 0. The performance penalty of the SIV version should be negligible,
    while protecting about the very edge case of reusing an IV.

--- src/repository/storage.rs
+++ src/repository/storage.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use aes_gcm::{Aes256Gcm, Key as AesKey, KeyInit, Nonce, aead::Aead};
+use aes_gcm_siv::{Aes256GcmSiv, Key as AesKey, KeyInit, Nonce, aead::Aead};
 use anyhow::{Result, bail};
 use argon2::Argon2;
 use rand::TryRngCore;
@@ -95,8 +95,8 @@ impl SecureStorage {
 
     /// Encrypt data using AES-GCM
     pub fn encrypt_with_key(key: &[u8], data: &[u8]) -> Result<Vec<u8>> {
-        let key = AesKey::<Aes256Gcm>::from_slice(key);
-        let cipher = Aes256Gcm::new(key);
+        let key = AesKey::<Aes256GcmSiv>::from_slice(key);
+        let cipher = Aes256GcmSiv::new(key);
 
         // Generate a random nonce for each encryption
         let mut nonce = vec![0u8; AES_GCM_NONCE_LEN];
@@ -126,8 +126,8 @@ impl SecureStorage {
 
     /// Decrypt data using AES-GCM
     pub fn decrypt_with_key(key: &[u8], data: &[u8]) -> Result<Vec<u8>> {
-        let key = AesKey::<Aes256Gcm>::from_slice(key);
-        let cipher = Aes256Gcm::new(key);
+        let key = AesKey::<Aes256GcmSiv>::from_slice(key);
+        let cipher = Aes256GcmSiv::new(key);
 
         if data.len() < AES_GCM_NONCE_LEN {
             bail!("Decryption failed: invalid data");

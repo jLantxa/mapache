05c67916d837e7e9379719337b380e9c727620ee
Javier Lancha Vázquez <javier.lancha@gmail.com>
2025-07-30 21:40:03 +0200

    Don't allow mounting the repo on the repo path

--- src/commands/cmd_mount.rs
+++ src/commands/cmd_mount.rs
@@ -16,12 +16,12 @@
 
 use std::path::PathBuf;
 
-use anyhow::Result;
+use anyhow::{Result, bail};
 use clap::Args;
 use colored::Colorize;
 
 use crate::{
-    backend::new_backend_with_prompt,
+    backend::{BackendUrl, new_backend_with_prompt},
     commands::{GlobalArgs, cleanup::CleanupHandler},
     fuse::fs::MapacheFS,
     repository::repo::{RepoConfig, Repository},
@@ -42,6 +42,14 @@ pub struct CmdArgs {
 }
 
 pub fn run(global_args: &GlobalArgs, args: &CmdArgs) -> Result<()> {
+    // Don't allow mounting on the repo path
+    let cannonical_mountpoint = std::fs::canonicalize(args.mountpoint.clone())?;
+    if let BackendUrl::Local(repo_path) = BackendUrl::from(&global_args.repo)? {
+        if cannonical_mountpoint == repo_path.canonicalize()? {
+            bail!("Cannot mount the repository on itself");
+        }
+    }
+
     let pass = utils::get_password_from_file(&global_args.password_file)?;
     let backend = new_backend_with_prompt(global_args, true)?;
 
@@ -52,18 +60,20 @@ pub fn run(global_args: &GlobalArgs, args: &CmdArgs) -> Result<()> {
         Repository::try_open_with_lock(pass, global_args.key.as_ref(), backend, config, false)?;
 
     // Listen for CTRL + C to unmount.
-    let mpoint = args.mountpoint.clone();
+    let mpoint = cannonical_mountpoint.clone();
+    let lock_handle_clone = lock_handle.clone();
     let _cleanup_handler = CleanupHandler::new(move || {
         let _ = MapacheFS::unmount(&mpoint);
+        let _ = lock_handle_clone.write().unlock();
     })?;
 
-    ui::cli::log!("Mounting repository in {}", &args.mountpoint.display());
+    ui::cli::log!("Mounting repository in {}", cannonical_mountpoint.display());
     ui::cli::log!(
         "Press {} to finish or unmount the filesystem manually.",
         "Ctrl+C".bold()
     );
     unsafe {
-        MapacheFS::mount(repo, &args.mountpoint, args.allow_other)?;
+        MapacheFS::mount(repo, &cannonical_mountpoint, args.allow_other)?;
     }
 
     Ok(())

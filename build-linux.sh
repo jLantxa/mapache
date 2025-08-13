c29ad5a86de4cb04ca4ad424967326c1fa2f67e0
Javier Lancha Vázquez <javier.lancha@gmail.com>
2025-08-13 23:04:53 +0200

    Invalidate docker image cache to rebuild with every execution

--- build-linux.sh
+++ build-linux.sh
@@ -4,9 +4,12 @@ IMAGE_NAME="mapache-builder"
 CONTAINER_NAME="mapache-extract-container"
 EXECUTABLE_NAME="mapache_linux_x64"
 BUILD_PATH="docker/build"
+REF=${1:-"main"}
+
+echo "Using Git ref: $REF"
 
 echo "Building Docker image..."
-docker build -t $IMAGE_NAME -f docker/Dockerfile .
+docker build --build-arg CACHE_BREAKER=$(date +%s) --build-arg GIT_REF=$REF -t $IMAGE_NAME -f docker/Dockerfile .
 
 echo "Creating container..."
 docker create --name $CONTAINER_NAME $IMAGE_NAME

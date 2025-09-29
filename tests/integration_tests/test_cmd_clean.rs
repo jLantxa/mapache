223c3574ab4be42a5c9fde8a901ad8121a4a88f5
Javier Lancha Vázquez <javier.lancha@gmail.com>
2025-09-30 00:33:14 +0200

    test_clean_dry_run

--- tests/integration_tests/test_cmd_clean.rs
+++ tests/integration_tests/test_cmd_clean.rs
@@ -17,10 +17,11 @@
 #![cfg(test)]
 
 mod tests {
-    use std::path::PathBuf;
+    use std::{path::PathBuf, sync::Arc};
 
     use anyhow::{Context, Result};
     use mapache::{
+        backend::{self, localfs::LocalFS, read_backend_dir},
         commands::{self, GlobalArgs, UseSnapshot, cmd_clean, cmd_restore, cmd_snapshot},
         global::{defaults::DEFAULT_DEFAULT_PACK_SIZE_MIB, set_global_opts_with_args},
         repository::repo::Auth,
@@ -35,6 +36,9 @@ mod tests {
     };
 
     /// Just a very basic test to verify that GC does not break the repository.
+    /// This is to check against some early bugs in the garbage collector that
+    /// removed files that it shouldn't.
+    /// It does no harm keeping this test.
     #[test]
     fn test_gc_sanity_check() -> Result<()> {
         let tmp_dir = tempdir()?;
@@ -182,6 +186,128 @@ mod tests {
                 assert_eq!(std::fs::read(&restored_path)?, std::fs::read(&backup_path)?);
             }
         }
+
+        Ok(())
+    }
+
+    /// Run clean but dry-run. Nothing should change.
+    #[test]
+    fn test_clean_dry_run() -> Result<()> {
+        let tmp_dir = tempdir()?;
+        let tmp_path = tmp_dir.path();
+        let auth = Auth {
+            username: "mapachito".to_string(),
+            password: "password".to_string(),
+        };
+        let auth_file_path = tmp_path.join("auth");
+        std::fs::write(
+            &auth_file_path,
+            format!("{}\n{}", auth.username, auth.password),
+        )?;
+
+        let backup_data_path = test_utils::get_test_data_path(BACKUP_DATA_PATH);
+        let backup_data_tmp_path = tmp_path.join("backup");
+        test_utils::extract_tar_xz_archive(&backup_data_path, &backup_data_tmp_path)?;
+
+        let repo = String::from("repo");
+        let repo_path = tmp_path.join(&repo);
+
+        let global = GlobalArgs {
+            repo: repo_path.to_string_lossy().to_string(),
+            auth_file: Some(auth_file_path),
+            key: None,
+            quiet: true,
+            verbosity: None,
+            ssh_pubkey: None,
+            ssh_privatekey: None,
+            pack_size_mib: DEFAULT_DEFAULT_PACK_SIZE_MIB,
+        };
+        set_global_opts_with_args(&global);
+
+        // Init repo
+        init_repo(&auth, repo_path.clone())?;
+
+        // Run snapshot twice
+        let snapshot_args = cmd_snapshot::CmdArgs {
+            paths: vec![
+                backup_data_tmp_path.join("0"),
+                backup_data_tmp_path.join("1"),
+                backup_data_tmp_path.join("2"),
+                backup_data_tmp_path.join("file.txt"),
+            ],
+            as_root: false,
+            exclude: None,
+            tags_str: String::new(),
+            description: None,
+            rescan: false,
+            parent: UseSnapshot::Latest,
+            read_concurrency: 2,
+            write_concurrency: 5,
+            dry_run: false,
+        };
+        commands::cmd_snapshot::run(&global, &snapshot_args)
+            .with_context(|| "Failed to run cmd_snapshot (1/2)")?;
+
+        let snapshot_args = cmd_snapshot::CmdArgs {
+            paths: vec![
+                backup_data_tmp_path.join("0"),
+                backup_data_tmp_path.join("1"),
+                backup_data_tmp_path.join("2"),
+            ],
+            as_root: false,
+            exclude: None,
+            tags_str: String::new(),
+            description: None,
+            rescan: false,
+            parent: UseSnapshot::Latest,
+            read_concurrency: 2,
+            write_concurrency: 5,
+            dry_run: false,
+        };
+        commands::cmd_snapshot::run(&global, &snapshot_args)
+            .with_context(|| "Failed to run cmd_snapshot (2/2)")?;
+
+        // Keep the last snapshot
+        let forget_args = commands::cmd_forget::CmdArgs {
+            forget: Vec::new(),
+            keep_last: Some(1),
+            keep_within: None,
+            keep_yearly: None,
+            keep_monthly: None,
+            keep_weekly: None,
+            keep_daily: None,
+            run_gc: false,
+            dry_run: false,
+            tolerance: 0.0_f32,
+            tags_str: Some(String::new()),
+            keep_tags_str: Some(String::new()),
+            verify: true,
+        };
+        commands::cmd_forget::run(&global, &forget_args)
+            .with_context(|| "Failed to run cmd_forget")?;
+
+        // Run cmd_clean and compare the repositories (using backend readdir)
+        let backend = Arc::new(LocalFS::new(repo_path.clone()));
+        let pre_clean_nodes = backend::read_backend_dir(backend.as_ref(), &PathBuf::new())?;
+        let gc_args = cmd_clean::CmdArgs {
+            tolerance: 0.0_f32,
+            dry_run: true, // DRY-RUN !
+            verify: true,
+        };
+        commands::cmd_clean::run(&global, &gc_args).with_context(|| "Failed to run cmd_gc")?;
+        let post_clean_nodes = read_backend_dir(backend.as_ref(), &PathBuf::new())?;
+        assert_eq!(pre_clean_nodes, post_clean_nodes);
+
+        // Now the same, but without dry-run, the repo changes
+        let pre_clean_nodes = backend::read_backend_dir(backend.as_ref(), &PathBuf::new())?;
+        let gc_args = cmd_clean::CmdArgs {
+            tolerance: 0.0_f32,
+            dry_run: false, // No dry-run
+            verify: true,
+        };
+        commands::cmd_clean::run(&global, &gc_args).with_context(|| "Failed to run cmd_gc")?;
+        let post_clean_nodes = read_backend_dir(backend.as_ref(), &PathBuf::new())?;
+        assert_ne!(pre_clean_nodes, post_clean_nodes);
 
         Ok(())
     }
